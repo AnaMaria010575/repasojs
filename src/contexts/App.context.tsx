08fb0f31c6de12ed11832d095607b0f5c0b581b4
AnaMaria010575 <139770906+AnaMaria010575@users.noreply.github.com>
2023-07-18 13:56:17 +0200

    Update App.context.tsx

--- src/contexts/App.context.tsx
+++ src/contexts/App.context.tsx
@@ -1,10 +1,22 @@
 
 import React, { useState } from "react";
 
+interface user {
+
+    name: string;
+    email: string;
+    role: string;
+    status: boolean;
+    google: boolean;
+    vid: string;
+}
+
+
 interface AppContextI {
     token: string,
+    user: User | null,
     setToken: (value: string) => void
-
+    setUser: (value: User) => void
 }
 
 export const AppContext = React.createContext({}) as AppContextI;
@@ -12,9 +24,15 @@ export const AppContext = React.createContext({}) as AppContextI;
 export const Provider = () => ({ Children }: any) => {
 
     const [token, setToken] = useState<string>('');
-    const values = {
-        token,
-        setToken
+    const [user, setUser] = useState<User | null>(null);
+        
+        const value = {
+
+            token,
+            setToken,
+            user,
+            setUser
+        }
 
 
 return(<AppContext.Provider value={ values } > { children }</AppContext.Provider >.Provider >)
