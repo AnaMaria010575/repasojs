8065a0ff27a17963ce82981d7752486a625e973e
AnaMaria010575 <139770906+AnaMaria010575@users.noreply.github.com>
2023-07-20 09:03:04 +0200

    last changes bgcolor, photo, header

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,11 @@
 
 import ReactDOM from 'react-dom/client'
 import './index.css'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import { AppRoutes } from './routes/App.routes.tsx'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <BrowserRouter>
+  <HashRouter>
     <AppRoutes />
-  </BrowserRouter>
+  </HashRouter>
 )
